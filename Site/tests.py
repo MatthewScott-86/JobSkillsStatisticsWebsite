68506d1669900102568b8c50b1a454521fe6b3e9
Amal R Krishna <amalrkrishna94@gmail.com>
2018-03-25 16:24:02 -0400

    added unit test cases

--- Site/tests.py
+++ Site/tests.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from django.test import TestCase
 
 import unittest
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
@@ -13,17 +14,79 @@ class PythonOrgSearch(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
 
-
     def test_landing_page_title(self):
-        print("DEBUG 1")
         driver = self.driver
         driver.get("http://0.0.0.0:8090")
-        #print(driver)
         self.assertIn("CS673", driver.title)
-        #elem = driver.find_element_by_name("q")
-        #elem.send_keys("pycon")
-        #elem.send_keys(Keys.RETURN)
         assert "Job Statistics Portal" in driver.page_source
+    
+    def test_landing_page_has_proper_title(self):
+        driver = self.driver
+        driver.get("http://0.0.0.0:8090")
+        self.assertIn("CS673", driver.title)
+        assert "Job Statistics Portal" in driver.page_source
+
+    def test_landing_page_has_indeed_menu(self):
+        driver = self.driver
+        driver.get("http://0.0.0.0:8090")
+        self.assertIn("CS673", driver.title)
+        soup = BeautifulSoup(driver.page_source,"html.parser")
+        links = []
+        for link in soup.find_all('a'):
+            links+= link
+        assert "Indeed" in  links
+
+    def test_landing_page_has_indeed_menu(self):
+        driver = self.driver
+        driver.get("http://0.0.0.0:8090")
+        self.assertIn("CS673", driver.title)
+        soup = BeautifulSoup(driver.page_source,"html.parser")
+        links = []
+        for link in soup.find_all('a'):
+            links+= link
+        print(links)
+        assert "Indeed" in  links
+        
+    def test_landing_page_has_glassdoor_menu(self):
+        driver = self.driver
+        driver.get("http://0.0.0.0:8090")
+        self.assertIn("CS673", driver.title)
+        soup = BeautifulSoup(driver.page_source,"html.parser")
+        links = []
+        for link in soup.find_all('a'):
+            links+= link
+        assert "Glassdoor" in  links
+    
+    def test_landing_page_has_home_menu(self):
+        driver = self.driver
+        driver.get("http://0.0.0.0:8090")
+        self.assertIn("CS673", driver.title)
+        soup = BeautifulSoup(driver.page_source,"html.parser")
+        links = []
+        for link in soup.find_all('a'):
+            links+= link
+        assert "Home" in  links
+
+    def test_landing_page_has_compare_jobs_menu(self):
+        driver = self.driver
+        driver.get("http://0.0.0.0:8090")
+        self.assertIn("CS673", driver.title)
+        soup = BeautifulSoup(driver.page_source,"html.parser")
+        links = []
+        for link in soup.find_all('a'):
+            links+= link
+        assert "Compare Jobs" in  links
+
+    
+    def test_landing_page_has_database_menu(self):
+        driver = self.driver
+        driver.get("http://0.0.0.0:8090")
+        self.assertIn("CS673", driver.title)
+        soup = BeautifulSoup(driver.page_source,"html.parser")
+        links = []
+        for link in soup.find_all('a'):
+            links+= link
+        assert "Database" in  links
 
 
     def tearDown(self):
