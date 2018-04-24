460a3737a84049922d900d0bb04cad69983cca4c
Amal R Krishna <amalrkrishna94@gmail.com>
2018-04-24 18:48:35 -0400

    integration first

--- Site/tests.py
+++ Site/tests.py
@@ -7,6 +7,9 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class PythonOrgSearch(unittest.TestCase):
@@ -14,117 +17,131 @@ class PythonOrgSearch(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.PhantomJS()
 
-    def test_landing_page_title(self):
-        driver = self.driver
-        driver.get("http://0.0.0.0:8090")
-        print(driver)
-        self.assertIn("Bird's Eye Statistics", driver.title)
-        assert "Job Statistics Portal" in driver.page_source
+    # def test_landing_page_title(self):
+    #     driver = self.driver
+    #     driver.get("http://0.0.0.0:8090")
+    #     print(driver)
+    #     self.assertIn("Bird's Eye Statistics", driver.title)
+    #     assert "Job Statistics Portal" in driver.page_source
     
-    def test_landing_page_has_proper_title(self):
-        driver = self.driver
-        driver.get("http://0.0.0.0:8090")
-        assert "Job Statistics Portal" in driver.page_source
+    # def test_landing_page_has_proper_title(self):
+    #     driver = self.driver
+    #     driver.get("http://0.0.0.0:8090")
+    #     assert "Job Statistics Portal" in driver.page_source
 
-    def test_landing_page_has_indeed_menu(self):
-        driver = self.driver
-        driver.get("http://0.0.0.0:8090")
-        soup = BeautifulSoup(driver.page_source,"html.parser")
-        links = []
-        for link in soup.find_all('a'):
-            links+= link
-        assert "Indeed" in  links
+    # def test_landing_page_has_indeed_menu(self):
+    #     driver = self.driver
+    #     driver.get("http://0.0.0.0:8090")
+    #     soup = BeautifulSoup(driver.page_source,"html.parser")
+    #     links = []
+    #     for link in soup.find_all('a'):
+    #         links+= link
+    #     assert "Indeed" in  links
         
-    def test_landing_page_has_glassdoor_menu(self):
-        driver = self.driver
-        driver.get("http://0.0.0.0:8090")
-        soup = BeautifulSoup(driver.page_source,"html.parser")
-        links = []
-        for link in soup.find_all('a'):
-            links+= link
-        assert "Glassdoor" in  links
+    # def test_landing_page_has_glassdoor_menu(self):
+    #     driver = self.driver
+    #     driver.get("http://0.0.0.0:8090")
+    #     soup = BeautifulSoup(driver.page_source,"html.parser")
+    #     links = []
+    #     for link in soup.find_all('a'):
+    #         links+= link
+    #     assert "Glassdoor" in  links
     
-    def test_landing_page_has_home_menu(self):
-        driver = self.driver
-        driver.get("http://0.0.0.0:8090")
-        soup = BeautifulSoup(driver.page_source,"html.parser")
-        links = []
-        for link in soup.find_all('a'):
-            links+= link
-        assert "Home" in  links
+    # def test_landing_page_has_home_menu(self):
+    #     driver = self.driver
+    #     driver.get("http://0.0.0.0:8090")
+    #     soup = BeautifulSoup(driver.page_source,"html.parser")
+    #     links = []
+    #     for link in soup.find_all('a'):
+    #         links+= link
+    #     assert "Home" in  links
 
-    def test_landing_page_has_compare_jobs_menu(self):
-        driver = self.driver
-        driver.get("http://0.0.0.0:8090")
-        soup = BeautifulSoup(driver.page_source,"html.parser")
-        links = []
-        for link in soup.find_all('a'):
-            links+= link
-        assert "Compare Jobs" in  links
-
+    # def test_landing_page_has_compare_jobs_menu(self):
+    #     driver = self.driver
+    #     driver.get("http://0.0.0.0:8090")
+    #     soup = BeautifulSoup(driver.page_source,"html.parser")
+    #     links = []
+    #     for link in soup.find_all('a'):
+    #         links+= link
+    #     assert "Compare Jobs" in  links
+   
+    # def test_landing_page_has_database_menu(self):
+    #     driver = self.driver
+    #     driver.get("http://0.0.0.0:8090")
+    #     soup = BeautifulSoup(driver.page_source,"html.parser")
+    #     links = []
+    #     for link in soup.find_all('a'):
+    #         links+= link
+    #     assert "Database" in  links
     
-    def test_landing_page_has_database_menu(self):
-        driver = self.driver
-        driver.get("http://0.0.0.0:8090")
-        soup = BeautifulSoup(driver.page_source,"html.parser")
-        links = []
-        for link in soup.find_all('a'):
-            links+= link
-        assert "Database" in  links
-    
-    def test_indeed_page_has_city_menu(self):
-        driver = self.driver
-        driver.get("http://0.0.0.0:8090/indeed")
-        soup = BeautifulSoup(driver.page_source,"html.parser")
-        links = []
-        for link in soup.find_all('select'):
-            links+= link
-        assert next((True for link in links if "Which City?" in link), False)
+    # def test_indeed_page_has_city_menu(self):
+    #     driver = self.driver
+    #     driver.get("http://0.0.0.0:8090/indeed")
+    #     soup = BeautifulSoup(driver.page_source,"html.parser")
+    #     links = []
+    #     for link in soup.find_all('select'):
+    #         links+= link
+    #     assert next((True for link in links if "Which City?" in link), False)
 
-    def test_indeed_page_has_job_menu(self):
-        driver = self.driver
-        driver.get("http://0.0.0.0:8090/indeed")
-        soup = BeautifulSoup(driver.page_source,"html.parser")
-        links = []
-        for link in soup.find_all('select'):
-            links+= link
-        assert next((True for link in links if "Do you have a job?" in link), False)
+    # def test_indeed_page_has_job_menu(self):
+    #     driver = self.driver
+    #     driver.get("http://0.0.0.0:8090/indeed")
+    #     soup = BeautifulSoup(driver.page_source,"html.parser")
+    #     links = []
+    #     for link in soup.find_all('select'):
+    #         links+= link
+    #     assert next((True for link in links if "Do you have a job?" in link), False)
 
-    def test_compare_page_has_first_job_menu(self):
-        driver = self.driver
-        driver.get("http://0.0.0.0:8090/indeed_compare")
-        soup = BeautifulSoup(driver.page_source,"html.parser")
-        links = []
-        for link in soup.find_all('select'):
-            links+= link
-        assert next((True for link in links if "Choose a job" in link), False)
+    # def test_compare_page_has_first_job_menu(self):
+    #     driver = self.driver
+    #     driver.get("http://0.0.0.0:8090/indeed_compare")
+    #     soup = BeautifulSoup(driver.page_source,"html.parser")
+    #     links = []
+    #     for link in soup.find_all('select'):
+    #         links+= link
+    #     assert next((True for link in links if "Choose a job" in link), False)
 
-    def test_compare_page_has_2nd_job_menu(self):
-        driver = self.driver
-        driver.get("http://0.0.0.0:8090/indeed_compare")
-        soup = BeautifulSoup(driver.page_source,"html.parser")
-        links = []
-        for link in soup.find_all('select'):
-            links+= link
-        assert next((True for link in links if "Choose a second job" in link), False)
+    # def test_compare_page_has_2nd_job_menu(self):
+    #     driver = self.driver
+    #     driver.get("http://0.0.0.0:8090/indeed_compare")
+    #     soup = BeautifulSoup(driver.page_source,"html.parser")
+    #     links = []
+    #     for link in soup.find_all('select'):
+    #         links+= link
+    #     assert next((True for link in links if "Choose a second job" in link), False)
 
-    def test_glassdoor_page_has_gen_stat_menu(self):
-        driver = self.driver
-        driver.get("http://0.0.0.0:8090/glassdoor")
-        soup = BeautifulSoup(driver.page_source,"html.parser")
-        links = []
-        for link in soup.find_all('select'):
-            links+= link
-        assert next((True for link in links if "General Statistics" in link), False)
+    # def test_glassdoor_page_has_gen_stat_menu(self):
+    #     driver = self.driver
+    #     driver.get("http://0.0.0.0:8090/glassdoor")
+    #     soup = BeautifulSoup(driver.page_source,"html.parser")
+    #     links = []
+    #     for link in soup.find_all('select'):
+    #         links+= link
+    #     assert next((True for link in links if "General Statistics" in link), False)
 
-    def test_glassdoor_page_has_box_plot_menu(self):
+    # def test_glassdoor_page_has_box_plot_menu(self):
+    #     driver = self.driver
+    #     driver.get("http://0.0.0.0:8090/glassdoor")
+    #     soup = BeautifulSoup(driver.page_source,"html.parser")
+    #     links = []
+    #     for link in soup.find_all('select'):
+    #         links+= link
+    #     assert next((True for link in links if "Box Plot" in link), False)
+
+    def test_landing_to_indeed(self):
         driver = self.driver
-        driver.get("http://0.0.0.0:8090/glassdoor")
-        soup = BeautifulSoup(driver.page_source,"html.parser")
-        links = []
-        for link in soup.find_all('select'):
-            links+= link
-        assert next((True for link in links if "Box Plot" in link), False)
+        driver.get("http://0.0.0.0:8090/")
+        indeed_button = driver.find_element_by_name("indeed_button")
+        print(indeed_button)
+        indeed_button.click()
+        modal = driver.find_elements_by_id("job")
+        print(modal[0])
+        WebDriverWait(driver, 10).until(EC.visibility_of(modal[0]))
+        self.assertTrue(modal[0].is_displayed())
+
+        #password_input = self.selenium.find_element_by_name("password")
+        #password_input.send_keys(‘secret’)
+        #driver.find_element_by_xpath(‘//input[@value=”Log in”]’).click()
 
     def tearDown(self):
         self.driver.close()
