0a95acb56900a7d8bdbd338611d3601a9fb08df7
Amal R Krishna <amalrkrishna94@gmail.com>
2018-04-25 17:00:17 -0400

    heroku

--- EmploymentSkillsStatisticsProject/settings.py
+++ EmploymentSkillsStatisticsProject/settings.py
@@ -131,3 +131,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
