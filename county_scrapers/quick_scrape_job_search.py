597f075f95e6b30ea3bdf33613c6f7695a28ca32
patryan117 <patryan117@gmail.com>
2018-04-25 20:48:25 -0400

    Final Text Size and Spacing

--- county_scrapers/quick_scrape_job_search.py
+++ county_scrapers/quick_scrape_job_search.py
@@ -109,7 +109,7 @@ def make_salary_donut_chart(dataframe):
                         "size": 15
                     },
                     "showarrow": False,
-                    "text": "Salaries",
+                    "text": "Salary",
                 },
             ]
         }
@@ -150,7 +150,7 @@ def make_location_donut_chart(dataframe):
                         "size": 15
                     },
                     "showarrow": False,
-                    "text": "Locations",
+                    "text": "Location",
                 },
             ]
         }
@@ -190,7 +190,7 @@ def make_company_donut_chart(dataframe):
                         "size": 15
                     },
                     "showarrow": False,
-                    "text": "Companies",
+                    "text": "Company",
                 },
             ]
         }
